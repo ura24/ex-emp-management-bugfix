3aaf858a82b0e157718fac2f58f3ebbd0e5f00cd
Yuto Urashima <yuto.urashima24@gmail.com>
2023-08-10 16:54:23 +0900

    [修正] 5-4

--- src/main/java/com/example/domain/LoginAdministrator.java
+++ src/main/java/com/example/domain/LoginAdministrator.java
@@ -8,27 +8,27 @@ import org.springframework.security.core.userdetails.User;
 /**
  * 管理者のログイン情報を返すエンティティ
  */
-public class LoginAdministrator  extends User{
+public class LoginAdministrator extends User {
 
 	/** 管理者情報 */
-	private final Administrator admin;
+	private final Administrator administrator;
 
 	/**
 	 * 管理者情報に、認可用ロールを設定
 	 * @param admin 管理者情報
 	 * @param authorities 権限情報リスト
 	 */
-	public LoginAdministrator(Administrator admin, Collection<? extends GrantedAuthority> authorities) {
-		super(admin.getMailAddress(), admin.getPassword(), authorities);
-		this.admin = admin;
+	public LoginAdministrator(Administrator administrator, Collection<GrantedAuthority> authorityList) {
+		super(administrator.getMailAddress(), administrator.getPassword(), authorityList);
+		this.administrator = administrator;
 	}
 
 	/**
 	 * 管理者情報を返す
 	 * @return 管理者情報
 	 */
-	public Administrator geAdministrator() {
-		return admin;
+	public Administrator getAdministrator() {
+		return administrator;
 	}
 	
 }
